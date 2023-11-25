7f8292b1ed7ae0b4e9b55febd3c9a47c99c91bb4
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2023-11-25 09:55:46 -0600

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export CLASSPATH=.:/usr/irissys/dev/java/lib/1.8/*
+
 echo "Building..."
 cd /opt/irisapp
 javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.*.jar -d . java/ReadExcel.java
