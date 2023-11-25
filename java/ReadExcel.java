22c092dc8f017ddfe5481839c2f058ea244752bc
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2023-11-24 19:20:13 -0600

    Create ReadExcel.java

---
+++ java/ReadExcel.java
@@ -0,0 +1,55 @@
+package reader;
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.CellType;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ReadExcel {
+
+    private String inputFile;
+
+    public void setInputFile(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public void read() throws IOException  {
+        File inputWorkbook = new File(inputFile);
+        Workbook w;
+        try {
+            w = Workbook.getWorkbook(inputWorkbook);
+            // Get the first sheet
+            Sheet sheet = w.getSheet(0);
+            // Loop over columns and lines
+            for (int col = 0; col < sheet.getColumns(); col++) {
+                for (int row = 0; row < sheet.getRows(); row++) {
+                    Cell cell = sheet.getCell(col, row);
+                    CellType type = cell.getType();
+                    if (type == CellType.LABEL) {
+                        System.out.println("I got a label "
+                                + cell.getContents());
+                    }
+
+                    if (type == CellType.NUMBER) {
+                        System.out.println("I got a number "
+                                + cell.getContents());
+                    }
+
+                }
+            }
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ReadExcel test = new ReadExcel();
+        test.setInputFile("/opt/irisapp/excel/money.xls");
+        test.read();
+    }
+
+}
