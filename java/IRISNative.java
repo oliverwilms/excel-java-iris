27e2b1494f11a7d75dbb7d28ab6bc9813fcb3548
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2023-11-26 14:36:12 -0600

    Update IRISNative.java

--- java/IRISNative.java
+++ java/IRISNative.java
@@ -43,12 +43,11 @@ public class IRISNative {
             Sheet sheet = w.getSheet(0);
             // Loop over rows and columns
             for (int row = 0; row < sheet.getRows(); row++) {
-                //ArrayList<String> myRow = new ArrayList<String>(); // Create an ArrayList object
                 for (int col = 0; col < sheet.getColumns(); col++) {
                     Cell cell = sheet.getCell(col, row);
                     CellType type = cell.getType();
                     if (type == CellType.LABEL) {
-                        System.out.println("I got a label " + cell.getContents());
+                        // System.out.println("I got a label " + cell.getContents());
                         String myLabel = cell.getContents();
 			// ObjectScript equivalent: set ^excel("1",row,col) = ...
                         iris.set(myLabel,"^excel","1",row,col);
@@ -56,7 +55,7 @@ public class IRISNative {
                     }
 
                     if (type == CellType.NUMBER) {
-                        System.out.println("I got a number " + cell.getContents());
+                        // System.out.println("I got a number " + cell.getContents());
                         String myNumber = cell.getContents();
        			// ObjectScript equivalent: set ^excel("1",row,col) = ...
                         iris.set(myNumber,"^excel","1",row,col);
@@ -75,45 +74,6 @@ public class IRISNative {
         }
 
 
-            System.out.println("[1. Setting and getting a global]");
-
-            // setting and getting a global
-            // ObjectScript equivalent: set ^testglobal("1") = 8888
-            iris.set(8888,"^testglobal","1");
-            // ObjectScript equivalent: set globalValue = $get(^testglobal("1"))
-            Integer globalValue = iris.getInteger("^testglobal","1");
-
-            System.out.println("The value of ^testglobal(1) is " + globalValue);
-            System.out.println();
-
-
-
-            System.out.println("[2. Iterating over a global]");
-
-            // modify global to iterate over
-            // ObjectScript equivalent: set ^testglobal("1") = 8888
-            // ObjectScript equivalent: set ^testglobal("2") = 9999
-            iris.set(8888,"^testglobal","1");
-            iris.set(9999,"^testglobal","2");
-
-            // iterate over all nodes forwards
-            IRISIterator subscriptIter = iris.getIRISIterator("^testglobal");
-            System.out.println("walk forwards");
-            while (subscriptIter.hasNext()) {
-                String subscript = subscriptIter.next();
-                System.out.println("subscript="+subscript+", value="+subscriptIter.getValue());
-            }
-
-            System.out.println();
-
-            System.out.println("[3. Calling a class method]");
-
-            // calling a class method
-            // ObjectScript equivalent: set returnValue = ##class(%Library.Utility).Date(5)
-            String returnValue = iris.classMethodString("%Library.Utility","Date",5);
-            System.out.println(returnValue);
-
-            System.out.println();
 
             // close connection and IRIS object
             iris.close();
